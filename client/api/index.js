77ab80891435a34abe9b3c8e12322f3587879266
Manik <manik@campusconnect.app>
2026-09-09 00:33:32 +0530

    fix: remove duplicate client/api and client/vercel.json causing wrong function routing

--- client/api/index.js
+++
@@ -1,6 +0,0 @@
-// Vercel serverless function entry point
-// Loads the pre-bundled Express app
-const mod = require('./_app.cjs');
-// esbuild CJS bundle wraps ESM default export in .default
-const app = mod.default || mod;
-module.exports = app;
